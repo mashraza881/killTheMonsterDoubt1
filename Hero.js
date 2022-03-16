bdf040e3c258ca87caf86261549cc8f47ae62249
mashraza881 <101043312+mashraza881@users.noreply.github.com>
2022-03-16 15:07:21 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,47 @@
+class Hero {
+  constructor(x,y,r)
+	{
+		var options = { 
+			density: 1, 
+			frictionAir: 1
+		};
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("superhero1.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+		World.add(world, this.body);
+
+		if (hero.isTouching(monster)){
+ 
+			playSound("Monster Kill.wav"); 
+
+			    
+
+            textSize(30);
+            textAlign(CENTER, TOP);
+            textFont("forte");
+            fill("gray");
+            stroke("pink");
+            strokeWeight(5);
+            text("You win ",200,240);
+}
+
+		
+
+	}
+	
+	display()
+	{
+			
+			var heroPos=this.body.position;		
+			push()
+			translate(heroPos.x, heroPos.y-100);
+			rectMode(CENTER)
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r+150, this.r)
+			pop()
+			
+	}
+}
